06988a22a46572db53ab431e7cfc8242dda7ccbd
Ebrahim Emad <ebemad8@gmail.com>
2025-06-12 11:56:25 +0300

    adding airflow tasks umages

--- dags/dag1.py
+++ dags/dag1.py
@@ -1,48 +1,38 @@
-from airflow import DAG
-from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
+from pathlib import Path
 
-# Define default arguments for the DAG
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2025, 6, 11),  # Change as needed
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 
-# Define the DAG
-dag = DAG(
-    dag_id='dbt_workflowv8',
-    default_args=default_args,
-    description='Run dbt models using dbt Core',
-    schedule='@daily',  # Run daily
+from dbt_airflow.core.config import DbtAirflowConfig, DbtProjectConfig, DbtProfileConfig
+from dbt_airflow.core.task_group import DbtTaskGroup
+from dbt_airflow.core.task import ExtraTask
+from dbt_airflow.operators.execution import ExecutionOperator
+
+
+with DAG(
+    dag_id='test_dag',
+    start_date=datetime(2021, 1, 1),
     catchup=False,
-)
-# Define the path to your dbt project
-DBT_PROJECT_DIR = "/opt/airflow/snowflake_data_project"
-DBT_PROFILES_DIR = "/opt/airflow/snowflake_data_project/profiles"  # Adjust as needed
-# Task 1: Run dbt models
-# Task 1: Run staging models
-dbt_run_staging = BashOperator(
-    task_id='dbt_run_staging',
-    bash_command=f'cd {DBT_PROJECT_DIR} && dbt run --profiles-dir {DBT_PROFILES_DIR} --select staging',
-    dag=dag,
-)
+    tags=['example'],
+    default_args={
+        'owner': 'airflow',
+        'retries': 1,
+        'retry_delay': timedelta(minutes=2),
+    },
+) as dag:
+   
+    tg = DbtTaskGroup(
+        group_id='dbt-company',
+        dbt_project_config=DbtProjectConfig(
+            project_path=Path('/opt/airflow/snowflake_data_project/'),
+            manifest_path=Path('/opt/airflow/snowflake_data_project/target/manifest.json'),
+        ),
+        dbt_profile_config=DbtProfileConfig(
+            profiles_path=Path('/opt/airflow/snowflake_data_project/profiles'),
+            target='dev',
+        )
+    )
 
-# Task 2: Run marts models
-dbt_run_marts = BashOperator(
-    task_id='dbt_run_marts',
-    bash_command=f'cd {DBT_PROJECT_DIR} && dbt run --profiles-dir {DBT_PROFILES_DIR} --select marts',
-    dag=dag,
-)
-
-# Task 3: Run dbt tests after all models are built
-dbt_test = BashOperator(
-    task_id='dbt_test',
-    bash_command=f'cd {DBT_PROJECT_DIR} && dbt test --profiles-dir {DBT_PROFILES_DIR}',
-    dag=dag,
-)
-
-# Define task dependencies
-dbt_run_staging >> dbt_run_marts >> dbt_test
+    tg 
